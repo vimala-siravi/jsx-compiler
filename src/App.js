1a51db7d8fa5f2c75718cea219ff4ea3325c6796
Vimala Sirvi <vimala.sirvi@sungardas.com>
2019-09-15 13:06:19 +0530

    JSX Compiler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      input: '/* Add your JSX here */',
+      output: '',
+      err: ''
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  update(e){
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: window.Babel.transform(code, {presets: ['es2015', 'react']})
+        .code,
+        err: ''
+      })
+    } catch (err) {
+      this.setState({err: err.message})
+    }
+  }
+
+  render(){
+    return(
+      <div>
+      <header>{this.state.err}</header>
+      <div className = "container">
+        <textarea
+          onChange = {this.update.bind(this)}
+          defaultValue = {this.state.input}
+        />
+        <pre>
+          {this.state.output}
+        </pre>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
